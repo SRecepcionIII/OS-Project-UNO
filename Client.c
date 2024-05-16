2d35ff2aa72a2142c6031f8de3dfa5dd7625dec2
JiuZhuuu <154327378+JiuZhuuu@users.noreply.github.com>
2024-05-16 16:30:21 +0800

    ss

--- Client.c
+++ Client.c
@@ -2,13 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
+//#include <arpa/inet.h>
 #include <sys/socket.h>
 
 #define PORT 8888
 #define SERVER_ADDR "127.0.0.1"
 
 // Uno game logic functions and structures (to be implemented)
+#define HAND_SIZE 7
+
+// Function to receive player's hand from server
+void receive_hand(int sock, char hand[][32]) {
+    char buffer[1024] = {0};
+    read(sock, buffer, sizeof(buffer));
+    printf("%s\n", buffer);
+    char *token = strtok(buffer, " ");
+    int i = 0;
+    while (token != NULL && i < HAND_SIZE) {
+        strcpy(hand[i], token);
+        token = strtok(NULL, " ");
+        i++;
+    }
+}
+
+// Function to send player's move to server
+void send_move(int sock, char *move) {
+    send(sock, move, strlen(move), 0);
+}
+
+// Function to check if a move is valid
+int is_valid_move(char *move, char *top_card) {
+    char *move_color = strtok(move, " ");
+    char *move_value = strtok(NULL, " ");
+
+    char *top_color = strtok(top_card, " ");
+    char *top_value = strtok(NULL, " ");
+
+    // Check if the move is a Wild card or a Wild Draw Four card
+    if (strcmp(move_color, "Wild") == 0 || strcmp(move_value, "Wild") == 0) {
+        return 1;
+    }
+
+    // Check if the move matches the color or value of the top card
+    if (strcmp(move_color, top_color) == 0 || strcmp(move_value, top_value) == 0) {
+        return 1;
+    }
+
+    return 0;
+}
 
 int main() {
     int sock = 0;
@@ -37,6 +78,77 @@ int main() {
     
     printf("Connected to server\n");
     
+    // Uno game setup
+    char hand[HAND_SIZE][32];
+
+    // Connect to server
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    
+    // Create client socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket creation error");
+        return -1;
+    }
+    
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, SERVER_ADDR, &serv_addr.sin_addr)<=0) {
+        perror("invalid address");
+        return -1;
+    }
+    
+    // Connect to server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("connection failed");
+        return -1;
+    }
+    
+    printf("Connected to server\n");
+
+    // Receive player's hand from server
+    receive_hand(sock, hand);
+
+    // Game loop
+    while (1) {
+        // Display player's hand
+        printf("Your hand: ");
+        for (int i = 0; i < HAND_SIZE; i++) {
+            printf("%s ", hand[i]);
+        }
+        printf("\n");
+
+        // Get player's move
+        char move[32];
+        printf("Enter your move: ");
+        scanf("%s", move);
+
+        // Send player's move to server
+        send_move(sock, move);
+
+        // Receive updated hand from server
+        receive_hand(sock, hand);
+
+        // Check for Uno
+        if (declare_uno(hand, HAND_SIZE)) {
+            printf("UNO!\n");
+        }
+        int i;
+        int end_game = 0;
+        for (i = 0; i < HAND_SIZE; i++) {
+            if (strcmp(hand[i], "") != 0) {
+                end_game = 1;
+                break;
+            }
+        }
+        if (!end_game) {
+            printf("You win!\n");
+            break;
+        }
+    }
+
     close(sock);
     return 0;
 }
--- Server.c
+++ Server.c
@@ -9,6 +9,115 @@
 #define MAX_CLIENTS 2
 
 // Uno game logic functions and structures (to be implemented)
+#define DECK_SIZE 108
+#define HAND_SIZE 7
+
+char *Color[] = {"Red", "Yellow", "Green", "Blue"};
+char *Value[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Skip", "Reverse", "+2", "Wild"};
+
+void shuffle_deck(char deck[][32], int size) {
+    srand(time(NULL)); // Seed the random number generator
+
+    // Fisher-Yates shuffle algorithm
+    for (int i = size - 1; i > 0; i--) {
+        int j = rand() % (i + 1); // Generate a random index between 0 and i
+        // Swap deck[i] and deck[j]
+        char temp[32];
+        strcpy(temp, deck[i]);
+        strcpy(deck[i], deck[j]);
+        strcpy(deck[j], temp);
+    }
+}
+
+void deal_cards(char deck[][32], char player_hands[MAX_CLIENTS][HAND_SIZE][32]) {
+    // Implement dealing cards logic
+    // For simplicity, we will just deal the first HAND_SIZE * MAX_CLIENTS cards
+    for (int i = 0; i < HAND_SIZE * MAX_CLIENTS; i++) {
+        strcpy(player_hands[i / HAND_SIZE][i % HAND_SIZE], deck[i]);
+    }
+}
+
+void display_hand(char *hand[], int size) {
+    printf("Your hand: ");
+    for (int i = 0; i < size; i++) {
+        printf("%s ", hand[i]);
+    }
+    printf("\n");
+}
+
+// Function to handle player's turn
+void player_turn(int client_socket, char hand[][32]) {
+    // Send player's hand to client
+    char buffer[1024] = {0};
+    sprintf(buffer, "Your hand: ");
+    for (int i = 0; i < HAND_SIZE; i++) {
+        strcat(buffer, hand[i]);
+        strcat(buffer, " ");
+    }
+    send(client_socket, buffer, strlen(buffer), 0);
+
+    // Receive player's move
+    char move[32];
+    recv(client_socket, move, sizeof(move), 0);
+    printf("Player move: %s\n", move);
+}
+
+// Function to check if a move is valid
+int is_valid_move(char *move, char *top_card) {
+    char *move_color = strtok(move, " ");
+    char *move_value = strtok(NULL, " ");
+
+    char *top_color = strtok(top_card, " ");
+    char *top_value = strtok(NULL, " ");
+
+    // Check if the move is a Wild card or a Wild Draw Four card
+    if (strcmp(move_color, "Wild") == 0 || strcmp(move_value, "Wild") == 0) {
+        return 1;
+    }
+
+    // Check if the move matches the color or value of the top card
+    if (strcmp(move_color, top_color) == 0 || strcmp(move_value, top_value) == 0) {
+        return 1;
+    }
+
+    return 0;
+}
+
+// Function to execute player's move
+void execute_move(char *move, char *hand[], int *hand_size) {
+    // Remove the played card from hand
+    for (int i = 0; i < *hand_size; i++) {
+        if (strcmp(hand[i], move) == 0) {
+            for (int j = i; j < *hand_size - 1; j++) {
+                strcpy(hand[j], hand[j + 1]);
+            }
+            strcpy(hand[*hand_size - 1], "");
+            break;
+        }
+    }
+    (*hand_size)--;
+}
+
+// Function to handle player's draw card action
+void draw_card(char *deck[], char *hand[], int *hand_size) {
+    // Draw a card from the deck
+    strcpy(hand[*hand_size], deck[0]);
+    (*hand_size)++;
+    // Shift the deck to remove the drawn card
+    for (int i = 0; i < DECK_SIZE - 1; i++) {
+        strcpy(deck[i], deck[i + 1]);
+    }
+    strcpy(deck[DECK_SIZE - 1], "");
+}
+
+// Function to handle player's Uno declaration
+int declare_uno(char *hand[], int hand_size) {
+    // Check if player has only one card left
+    if (hand_size == 1) {
+        return 1;
+    }
+    return 0;
+}
 
 int main() {
     int server_fd, new_socket, client_sockets[MAX_CLIENTS] = {0};
@@ -47,6 +156,32 @@ int main() {
         printf("Client %d connected\n", i+1);
     }
     
+    // Uno game setup
+    char deck[DECK_SIZE][32];
+    char player_hands[MAX_CLIENTS][HAND_SIZE][32];
+
+    // Initialize and shuffle deck
+    shuffle_deck(deck, DECK_SIZE);
+
+    // Deal cards to players
+    deal_cards(deck, player_hands);
+
+    // Game loop
+    int client_sockets[MAX_CLIENTS] = {0};
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        // Accept incoming connections
+        if ((client_sockets[i] = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        printf("Client %d connected\n", i+1);
+
+        // Handle player's turn
+        char player_hand[HAND_SIZE][32];
+        deal_cards(NULL, player_hand);  // Placeholder for dealing cards
+        player_turn(client_sockets[i], player_hand);
+    }
+
     // Close all client sockets
     for (int i = 0; i < MAX_CLIENTS; i++) {
         close(client_sockets[i]);
